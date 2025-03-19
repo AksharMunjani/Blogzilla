c9a1b46a883748805365b807a7eaa6a889528b38
Jaymin Munjani <96972980+AksharMunjani@users.noreply.github.com>
2025-03-19 22:18:17 +0530

    images fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.vercel.app',
-        port: '',
-        pathname: '/api/media/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/api/media/**',
-      }
-    ],
+    domains: ['localhost:3000', 'blogzilla-akshar.vercel.app'],
   },
 }
 
